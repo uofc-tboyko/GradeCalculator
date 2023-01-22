35d76a0a93433dec0218e6cf2075f0c856048d5c
thoma <thoma@pajamamanlaptop>
2023-01-22 11:56:13 -0700

    Grade calculator first uploaded iteration

---
+++ src/application/GradeCalculatorController.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class GradeCalculatorController {
+
+	@FXML
+	private Slider quizSlider;
+    @FXML
+    private ChoiceBox<Integer> optPassedChoiceBox;
+
+    @FXML
+    private TextField projectGradeTextField;
+
+    @FXML
+    private ChoiceBox<Integer> challengesPassedChoiceBox;
+
+    @FXML
+    private Label textGradeDisplay;
+    
+    @FXML
+    void calculateGrade(ActionEvent event) {
+    	double courseGrade = 0;
+    	String projectGrade = projectGradeTextField.getText();
+    	double quizGrade = quizSlider.getValue();
+    	int chalPassed = challengesPassedChoiceBox.getValue();
+    	int optPassed = optPassedChoiceBox.getValue();
+    	
+    	double totalChalGrade = (optPassed + chalPassed)*5;//gives the total challenge grade as a percent
+
+    	courseGrade += Double.parseDouble(projectGrade)/2;//adds half of the project grade to the final grade
+    	courseGrade += (quizGrade*2.5);
+    	courseGrade += (totalChalGrade)/4;
+    	
+    	textGradeDisplay.setText(String.format("Your overall course grade is: %.2f", courseGrade));
+    }
+
+}
